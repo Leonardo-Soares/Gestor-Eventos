950993942a9f540374e585fd7e2fd7a652c3250f
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-02-28 13:38:14 -0300

    Ajuste de erro na view dos eventos

--- resources/views/eventos/eventos.blade.php
+++ resources/views/eventos/eventos.blade.php
@@ -3,46 +3,36 @@
 @extends('layouts.menu')
 
 @section('content')
-
-    <h1>Eventos em Belém</h1>
-
-    <h1>Busque um evento</h1>
-    <form action="/eventos" method="GET">
-        <input type="text" name="procurar" id="procurar" placeholder="Procure um evento">
-    </form>
-    @if ($procurar)
-        <h2>Buscando por: {{$procurar}}</h2>
-    @else
-        <h2>Próximos Eventos</h2>
-    @endif
-
-
-    @foreach ($eventos as $evento)
-        <ul>
-            <li>Título: {{ $evento->titulo }}</li>
-            <li>Cidade: {{ $evento->cidade }}</li>
-            <li>Endereço: {{ $evento->endereco }}</li>
-            <li>Dia: {{ date('d/m/Y', strtotime($evento->dia)) }}</li>
-            <li>Hora: {{ $evento->horario }}</li>
-            <li>Descrição: {{ $evento->descricao }}</li>
-            <h3>O evento conta com:</h3>
-           <ul id="items-list">
-                @foreach($evento => $items as $item)
-                    <li><ion-icon name="play-outline"></ion-icon> <span>{{ $item }}</span></li>
-                @endforeach
-            </ul>
-            <a href="eventos/detalhes/{{ $evento->id }}" class="btn btn-primary">Saber mais</a>
-        </ul>
-    @endforeach
-
-    @if (count($eventos) == 0 && $procurar)
-        <p> Não encontramos nenhum evento com <b>{{$procurar}}</b>  <br/>
-            Veja muito mais clicando em: <a href="/eventos">Ver Todos</a>
-        </p>
-    @elseif (count($eventos) == 0)
-        <p>Não há eventos disponíveis</p>
-    @if (count($eventos) == 0)
-        <p>Não temos eventos hoje</p>
-    @endif
-
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="/" method="GET">
+            <input type="text" id="procurar" name="procurar" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        @if ($procurar)
+            <h2>Buscando por: {{ $procurar }}</h2>
+        @else
+            <h2>Próximos Eventos</h2>
+            <p class="subtitle">Veja os eventos nos próximos dias</p>
+        @endif
+        <div id="cards-container" class="row">
+            @foreach ($eventos as $event)
+                <div class="card col-md-3">
+                    <img src="/img/eventos/{{ $event->image }}" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="/eventos/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+            @if (count($eventos) == 0 && $procurar)
+                <p>Não foi possível encontrar nenhum evento com {{ $procurar }}! <a href="/">Ver todos</a></p>
+            @elseif(count($eventos) == 0)
+                <p>Não há eventos disponíveis</p>
+            @endif
+        </div>
+    </div>
 @endsection
