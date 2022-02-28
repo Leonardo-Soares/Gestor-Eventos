37988c6ec47e51ae9090580401918f73eaf29072
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-02-28 14:16:37 -0300

    Ajuste para acessar detalhes do evento

--- resources/views/eventos/eventos.blade.php
+++ resources/views/eventos/eventos.blade.php
@@ -24,7 +24,7 @@
                         <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                         <h5 class="card-title">{{ $event->title }}</h5>
                         <p class="card-participants">X Participantes</p>
-                        <a href="/eventos/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                        <a href="/eventos/detalhes/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
                     </div>
                 </div>
             @endforeach
